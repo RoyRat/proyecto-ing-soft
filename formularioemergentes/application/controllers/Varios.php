af6e55ea2b3dc583779e54ccab8279a98b5e09c1
Diego Lascano <31676836+DallasBelt@users.noreply.github.com>
2020-08-02 13:20:45 -0500

    Update Varios.php
    Ajuste de indentación.

--- formularioemergentes/application/controllers/Varios.php
+++ formularioemergentes/application/controllers/Varios.php
@@ -5,30 +5,31 @@ class Varios extends CI_Controller{
         $this->load->model('mvarios');
     }
     
-     function ruta($urlEncoded){
+    function ruta($urlEncoded){
         $this->load->model('maprobar'); 
         $function = explode("--",base64_decode($urlEncoded));
-  	echo $this->$function[0]->$function[1]($function[2],$function[3],$function[4]);
-      }
+  	    echo $this->$function[0]->$function[1]($function[2],$function[3],$function[4]);
+    }
 
 	//Funcion para obtener los subgrupos en base a los grupos
-   function get_provincia(){
+    function get_provincia(){
         $LOC_PAIS=$this->input->post('pais');
-		if(!empty($LOC_PAIS) and $LOC_PAIS<>null){
-        $output = "";
-        $query = $this->db->query("select LOC_SECUENCIAL, LOC_DESCRIPCION 
-										FROM LOCALIZACION 
-										where LOC_NIVEL=2 
-										AND LOC_ESTADO=0 
-										AND LOC_PREDECESOR=$LOC_PAIS
-										order by LOC_DESCRIPCION");
-        $results = $query->result();
-        $output .='<option value="">Provincia..</option>';
-        foreach ($results as $result):
+        if(!empty($LOC_PAIS) and $LOC_PAIS<>null){
+            $output = "";
+            $query = $this->db->query("select LOC_SECUENCIAL, LOC_DESCRIPCION 
+                FROM LOCALIZACION 
+                where LOC_NIVEL=2 
+                AND LOC_ESTADO=0 
+                AND LOC_PREDECESOR=$LOC_PAIS
+                order by LOC_DESCRIPCION");
+            $results = $query->result();
+            $output .='<option value="">Provincia..</option>';
+            foreach ($results as $result):
                 $output .="<option value="."'".$result->LOC_SECUENCIAL."'".">".($result->LOC_DESCRIPCION)."</option>";
-        endforeach;
-        echo $output;
-		}else{
+            endforeach;
+            echo $output;
+		}
+        else{
 			$output = "";
 			$output .='<option value="">Provincia..</option>';
 			echo $output;
@@ -39,20 +40,21 @@ class Varios extends CI_Controller{
    function get_ciudad(){
         $LOC_PROVINCIA=$this->input->post('ciudad');
 		if(!empty($LOC_PROVINCIA) and $LOC_PROVINCIA<>null){
-        $output = "";
-        $query = $this->db->query("select LOC_SECUENCIAL, LOC_DESCRIPCION 
-										FROM LOCALIZACION 
-										where LOC_NIVEL=3 
-										AND LOC_ESTADO=0 
-										AND LOC_PREDECESOR=$LOC_PROVINCIA
-										order by LOC_DESCRIPCION");
-        $results = $query->result();
-        $output .='<option value="">Ciudad..</option>';
-        foreach ($results as $result):
-                $output .="<option value="."'".$result->LOC_SECUENCIAL."'".">".($result->LOC_DESCRIPCION)."</option>";
-        endforeach;
-        echo $output;
-		}else{
+            $output = "";
+            $query = $this->db->query("select LOC_SECUENCIAL, LOC_DESCRIPCION 
+                FROM LOCALIZACION 
+                where LOC_NIVEL=3 
+                AND LOC_ESTADO=0 
+                AND LOC_PREDECESOR=$LOC_PROVINCIA
+                order by LOC_DESCRIPCION");
+            $results = $query->result();
+            $output .='<option value="">Ciudad..</option>';
+            foreach ($results as $result):
+                    $output .="<option value="."'".$result->LOC_SECUENCIAL."'".">".($result->LOC_DESCRIPCION)."</option>";
+            endforeach;
+            echo $output;
+		}
+        else{
 			$output = "";
 			$output .='<option value="">Ciudad..</option>';
 			echo $output;
@@ -63,25 +65,24 @@ class Varios extends CI_Controller{
    function get_sector(){
         $LOC_CIUDAD=$this->input->post('sector');
 		if(!empty($LOC_CIUDAD) and $LOC_CIUDAD<>null){
-        $output = "";
-        $query = $this->db->query("select LOC_SECUENCIAL, LOC_DESCRIPCION 
-										FROM LOCALIZACION 
-										where LOC_NIVEL=4 
-										AND LOC_ESTADO=0 
-										AND LOC_PREDECESOR=$LOC_CIUDAD
-										order by LOC_DESCRIPCION");
-        $results = $query->result();
-        $output .='<option value="">Sector..</option>';
-        foreach ($results as $result):
-                $output .="<option value="."'".$result->LOC_SECUENCIAL."'".">".($result->LOC_DESCRIPCION)."</option>";
-        endforeach;
-        echo $output;
-		}else{
+            $output = "";
+            $query = $this->db->query("select LOC_SECUENCIAL, LOC_DESCRIPCION 
+                FROM LOCALIZACION 
+                where LOC_NIVEL=4 
+                AND LOC_ESTADO=0 
+                AND LOC_PREDECESOR=$LOC_CIUDAD
+                order by LOC_DESCRIPCION");
+            $results = $query->result();
+            $output .='<option value="">Sector..</option>';
+            foreach ($results as $result):
+                    $output .="<option value="."'".$result->LOC_SECUENCIAL."'".">".($result->LOC_DESCRIPCION)."</option>";
+            endforeach;
+            echo $output;
+		}
+        else{
 			$output = "";
 			$output .='<option value="">Sector..</option>';
 			echo $output;
 		}
    }
-
-      
 }
